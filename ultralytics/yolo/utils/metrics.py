4c61ca9017e403f1f7e9a41096d43b4c2732531d
ldbang <95518835+ldbang@users.noreply.github.com>
2023-01-18 18:05:12 +0800

    Update metrics.py

--- ultralytics/yolo/utils/metrics.py
+++ ultralytics/yolo/utils/metrics.py
@@ -63,7 +63,7 @@ def box_iou(box1, box2, eps=1e-7):
     return inter / ((a2 - a1).prod(2) + (b2 - b1).prod(2) - inter + eps)
 
 
-def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, SIoU = False, eps=1e-7):
+def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, SIoU = False,EIoU = False, eps=1e-7):
     # Returns Intersection over Union (IoU) of box1(1,4) to box2(n,4)
 
     # Get the coordinates of bounding boxes
@@ -87,7 +87,7 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, SIoU = F
 
     # IoU
     iou = inter / union
-    if CIoU or DIoU or GIoU or SIoU:
+    if CIoU or DIoU or GIoU or SIoU or EIoU:
         cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
         ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
         if CIoU or DIoU or EIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
