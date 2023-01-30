364da96d88df7142af6f63c3a475824ea26094a0
ldbang <95518835+ldbang@users.noreply.github.com>
2023-01-30 22:32:59 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -279,6 +279,68 @@ class C2f_CABM(nn.Module):
 #         return self.cv2(torch.cat(y, 1))
         return self.spatial_attention(self.channel_attention(self.cv2(torch.cat(y, 1))))
 
+
+
+
+# CBAM
+class ChannelAttention_2(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention_2, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.f1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu = nn.ReLU()
+        self.f2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.f2(self.relu(self.f1(self.avg_pool(x))))
+        max_out = self.f2(self.relu(self.f1(self.max_pool(x))))
+        out = self.sigmoid(avg_out + max_out)
+        return out
+class SpatialAttention_2(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention_2, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        # (特征图的大小-算子的size+2*padding)/步长+1
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        # 1*h*w
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        #2*h*w
+        x = self.conv(x)
+        #1*h*w
+        return self.sigmoid(x)
+class CBAM_2(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, ratio=16, kernel_size=7):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(CBAM_2, self).__init__()
+        self.channel_attention = ChannelAttention_2(c1, ratio)
+        self.spatial_attention = SpatialAttention_2(kernel_size)
+    def forward(self, x):
+        out = self.channel_attention(x) * x
+        # c*h*w
+        # c*h*w * 1*h*w
+        out = self.spatial_attention(out) * out
+        return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class ECA(nn.Module):
      def __init__(self, channel, k_size=3):
          super(ECA, self).__init__()
