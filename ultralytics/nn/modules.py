fad2b3843e72066e3e72bd8e6cc3a3bbdd7c3289
ldbang <95518835+ldbang@users.noreply.github.com>
2023-01-30 17:52:22 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -279,6 +279,31 @@ class C2f_CABM(nn.Module):
 #         return self.cv2(torch.cat(y, 1))
         return self.spatial_attention(self.channel_attention(self.cv2(torch.cat(y, 1))))
 
+ class ECA(nn.Module):
+     """Constructs a ECA module.
+     Args:
+         channel: Number of channels of the input feature map
+         k_size: Adaptive selection of kernel size
+     """
+     def __init__(self, channel, k_size=3):
+         super(ECA, self).__init__()
+         self.avg_pool = nn.AdaptiveAvgPool2d(1)
+         self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+         self.sigmoid = nn.Sigmoid()
+ 
+     def forward(self, x):
+         # feature descriptor on the global spatial information
+         y = self.avg_pool(x)
+ 
+         # Two different branches of ECA module
+         y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+ 
+         # Multi-scale information fusion
+         y = self.sigmoid(y)
+         x=x*y.expand_as(x)
+ 
+         return x * y.expand_as(x)
+
 class C1(nn.Module):
     # CSP Bottleneck with 1 convolution
     def __init__(self, c1, c2, n=1):  # ch_in, ch_out, number
