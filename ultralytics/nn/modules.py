adb46c99e1a414d749bf7568c6c1c08eb797c187
ldbang <95518835+ldbang@users.noreply.github.com>
2023-01-30 17:35:04 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -240,6 +240,23 @@ class CBAM(nn.Module):
     def forward(self, x):
         return self.spatial_attention(self.channel_attention(x))
 
+class C2f_CABM(nn.Module):
+    # CSP Bottleneck with 2 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+#         super().__init__()
+        super(C2f_CABM, self).__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.channel_attention = ChannelAttention(c2, 16)
+        self.spatial_attention = SpatialAttention(7)
+        
+    def forward(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+#         return self.cv2(torch.cat(y, 1))
+        return self.spatial_attention(self.channel_attention(self.cv2(torch.cat(y, 1))))
 
 class C1(nn.Module):
     # CSP Bottleneck with 1 convolution
