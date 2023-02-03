b7661172212e4f40c1265315d079dbbab0fcdac8
ldbang <95518835+ldbang@users.noreply.github.com>
2023-02-03 16:02:17 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -974,9 +974,20 @@ class RepConv(nn.Module):
             del self.rbr_dense
             self.rbr_dense = None
 
+# class RepBottleneck(nn.Module):
+#     # Standard bottleneck
+#     def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+#         super().__init__()
+#         c_ = int(c2 * e)  # hidden channels
+#         self.cv1 = Conv(c1, c_, 1, 1)
+#         self.cv2 = RepConv(c_, c2, 3, 1, g=g)
+#         self.add = shortcut and c1 == c2
+
+#     def forward(self, x):
+#         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 class RepBottleneck(nn.Module):
     # Standard bottleneck
-    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+    def __init__(self, c1, c2, shortcut=True, k=0,g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
@@ -985,8 +996,17 @@ class RepBottleneck(nn.Module):
 
     def forward(self, x):
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+class RepBottleneck__(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, k=0,g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = RepConv(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
 
-
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 class RepC3(nn.Module):
     # CSP Bottleneck with 3 convolutions
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
