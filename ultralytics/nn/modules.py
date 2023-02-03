5e451521788626f9eb9ce2057b5fd2905fbb4d4b
ldbang <95518835+ldbang@users.noreply.github.com>
2023-02-03 16:12:48 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -1028,7 +1028,7 @@ class RepC2f(nn.Module):
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
 #         self.m = nn.ModuleList(RepBottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
-        self.m = nn.ModuleList(RepBottleneck(self.c, self.c, shortcut, g, k=(3, 3), e=1.0) for _ in range(n))
+        self.m = nn.ModuleList(RepBottleneck(self.c, self.c, shortcut, g, k=0, e=1.0) for _ in range(n))
     def forward(self, x):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
