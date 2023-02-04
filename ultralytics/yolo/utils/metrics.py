04953d63c43438c095ce5341807d779513801699
ldbang <95518835+ldbang@users.noreply.github.com>
2023-02-04 21:21:36 +0800

    Update metrics.py

--- ultralytics/yolo/utils/metrics.py
+++ ultralytics/yolo/utils/metrics.py
@@ -123,7 +123,9 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, SIoU = F
             omiga_w = torch.abs(w1 - w2) / torch.max(w1, w2)
             omiga_h = torch.abs(h1 - h2) / torch.max(h1, h2)
             shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), 4) + torch.pow(1 - torch.exp(-1 * omiga_h), 4)
-            iou = iou - 0.5 * (distance_cost + shape_cost) #SIoU
+            
+#             iou = iou - 0.5 * (distance_cost + shape_cost) #SIoU
+            iou = iou - 0.51 * distance_cost -0.49* shape_cost) #SIoU        
 #             print("SIoU")
             return iou
         c_area = cw * ch + eps  # convex area
